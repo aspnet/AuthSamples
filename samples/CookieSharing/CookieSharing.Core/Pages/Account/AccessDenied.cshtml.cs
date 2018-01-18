da8561f68806fd464911b2c1e2abd02e1662e2bd
Luke Latham <1622880+guardrex@users.noreply.github.com>
2018-01-17 18:49:40 -0600

    Add cookie sample projects
    Add cookie sharing projects

---
+++ samples/CookieSharing/CookieSharing.Core/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CookieSharingCore.Pages.Account
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ samples/CookieSharing/CookieSharing.WithIdentity.Core/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CookieSharingWithIdentityCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
