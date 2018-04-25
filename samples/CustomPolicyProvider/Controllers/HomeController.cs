41b5f61c2992bb9420e08d119274a08fe83ce10d
Mike Rousos <Mike@MJRousos.com>
2018-04-25 12:11:58 -0400

    Make policy provider sample an MVC app and replace Values controller with a home controller (with views)

---
+++ samples/CustomPolicyProvider/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CustomPolicyProvider.Controllers
+{
+    // Sample actions to demonstrate the use of the [MinimumAgeAuthorize] attribute
+    [Controller]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // View protected with custom parameterized authorization policy
+        [MinimumAgeAuthorize(10)]
+        public IActionResult MinimumAge10()
+        {
+            return View();
+        }
+
+        // View protected with custom parameterized authorization policy
+        [MinimumAgeAuthorize(30)]
+        public IActionResult MinimumAge30()
+        {
+            return View();
+        }
+    }
+}
--- samples/CustomPolicyProvider/Startup.cs
+++ samples/CustomPolicyProvider/Startup.cs
@@ -22,7 +22,12 @@ namespace CustomPolicyProvider
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseMvc();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}");
+            });
         }
     }
 }
